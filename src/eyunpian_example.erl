77106d8766c8aac4f91737921e015f26081b9280
dcy <766320183@qq.com>
2016-10-19 20:00:45 +0800

    完善云片api

---
+++ src/eyunpian_example.erl
@@ -0,0 +1,35 @@
+-module(eyunpian_example).
+-compile(export_all).
+
+-define(TEXT, unicode:characters_to_binary("【XX科技】验证码1234，请您尽快验证，完成注册。如非本人操作请忽略。")).
+-define(TEXT1, unicode:characters_to_binary("【XX科技】验证码1234，请您尽快验证，重置密码。如非本人操作请忽略。")).
+-define(MOBILE, <<"15102025006">>).
+%-define(MOBILES, <<"15102025006,15102025006">>).
+-define(MOBILES, ["15102025006", "15102025006"]).
+
+single_send() ->
+    eyunpian_sms:single_send(?MOBILE, ?TEXT).
+
+batch_send() ->
+    eyunpian_sms:batch_send(?MOBILES, ?TEXT).
+
+multi_send() ->
+    eyunpian_sms:multi_send(?MOBILES, [?TEXT, ?TEXT1]).
+
+get_reply() ->
+    %eyunpian_sms:get_reply("15102025006", "2013-08-11 00:00:00", "2013-08-12 00:00:00").
+    Datas = #{apikey => "yunpian_api_key",
+              start_time => "2016-10-18 00:00:00",
+              end_time => "2016-10-20 00:00:00",
+              %mobile => "15102025006"
+              page_num => 1,
+              page_size => 20
+             },
+    eyunpian_sms:get_reply(Datas).
+
+
+get_record() ->
+    eyunpian_sms:get_record("15102025006", "2013-08-11 00:00:00", "2013-08-12 00:00:00").
+
+get_black_word() ->
+    eyunpian_sms:get_black_word("这是一条测试发票短信").
--- src/eyunpian_sms.erl
+++ src/eyunpian_sms.erl
@@ -1,20 +1,74 @@
 -module(eyunpian_sms).
 
 %%API
--export([]).
+-export([single_send/2, single_send/3,
+         batch_send/2, batch_send/3,
+         multi_send/2, multi_send/3,
+         get_reply/1, get_reply/3, get_reply/4, get_reply/6,
+         get_record/1, get_record/3, get_record/4,
+         get_black_word/1, get_black_word/2
+        ]).
 
 -export([]).
 
 -include_lib("eutil/include/eutil.hrl").
+-define(POOL, yunpian_sms).
 
-single_send() ->
-    ok.
+single_send(Mobile, Text) ->
+    Apikey = get_conf_api_key(),
+    single_send(Apikey, Mobile, Text).
 
-batch_send() ->
-    ok.
+single_send(Apikey, Mobile, Text) when is_binary(Mobile) andalso is_binary(Text) ->
+    Datas = #{apikey => Apikey, mobile => Mobile,
+              text => unicode:characters_to_binary(Text)},
+    URL = <<"http://sms.yunpian.com/v2/sms/single_send.json">>,
+    Result = eutil:http_post(URL, ?URLENCEDED_HEADS, Datas, [{pool, ?POOL}]),
+    case maps:get(<<"code">>, Result) of
+        ?SUCCESS_0 ->
+            ok;
+        Code ->
+            ?ERROR_MSG("eyunpian_sms single_send error, Mobile: ~p, Result: ~p", [Mobile, Result]),
+            {error, Code}
+    end.
 
-multi_send() ->
-    ok.
+
+batch_send(Mobiles, Text) ->
+    Apikey = get_conf_api_key(),
+    batch_send(Apikey, Mobiles, Text).
+
+batch_send(Apikey, Mobiles, Text) when is_list(Mobiles) ->
+    batch_send(Apikey, format_mobiles(Mobiles), Text);
+batch_send(Apikey, Mobiles, Text) when is_binary(Mobiles) andalso is_binary(Text) ->
+    Datas = #{apikey => Apikey, mobile => Mobiles, text => Text},
+    URL = <<"http://sms.yunpian.com/v2/sms/batch_send.json">>,
+    Result = eutil:http_post(URL, ?URLENCEDED_HEADS, Datas, [{pool, ?POOL}]),
+    case maps:get(<<"data">>, Result, undefined) of
+        undefined ->
+            ?ERROR_MSG("eyunpian_sms batch_send error, Mobiles: ~p, Result: ~p", [Mobiles, Result]),
+            {error, maps:get(<<"code">>, Result)};
+        _ ->
+            ok
+    end.
+
+
+multi_send(Mobiles, TextList) ->
+    Apikey = get_conf_api_key(),
+    multi_send(Apikey, Mobiles, TextList).
+
+multi_send(Apikey, Mobiles, Texts) when is_list(Mobiles) andalso is_list(Texts) ->
+    multi_send(Apikey, format_mobiles(Mobiles), format_texts(Texts));
+multi_send(Apikey, Mobiles, Texts) when is_binary(Mobiles) andalso is_binary(Texts) -> 
+    Datas = #{apikey => Apikey, mobile => Mobiles, text => Texts},
+    URL = <<"https://sms.yunpian.com/v2/sms/multi_send.json">>,
+    Result = eutil:http_post(URL, ?URLENCEDED_HEADS, Datas, [{pool, ?POOL}]),
+    case maps:get(<<"data">>, Result, undefined) of
+        undefined ->
+            ?ERROR_MSG("eyunpian_sms multi_send error, Mobiles: ~p, Result: ~p", [Mobiles, Result]),
+            {error, maps:get(<<"code">>, Result)};
+        _ ->
+            ok
+    end.
+
 
 pull_status() ->
     ok.
@@ -22,12 +76,64 @@ pull_status() ->
 pull_reply() ->
     ok.
 
-get_reply() ->
-    ok.
+get_reply(Mobile, StartTime, EndTime) ->
+    Apikey = get_conf_api_key(),
+    get_reply(Apikey, Mobile, StartTime, EndTime).
 
-get_record() ->
-    ok.
+get_reply(Apikey, Mobile, StartTime, EndTime) ->
+    get_reply(Apikey, Mobile, StartTime, EndTime, 1, 20).
 
-get_black_word() ->
-    ok.
+get_reply(Apikey, Mobile, StartTime, EndTime, PageNum, PageSize) ->
+    Datas = #{apikey => Apikey, start_time => StartTime, end_time => EndTime,
+              page_num => PageNum, page_size => PageSize, mobile => Mobile},
+    get_reply(Datas).
 
+get_reply(Datas) ->
+    URL = <<"https://sms.yunpian.com/v2/sms/get_reply.json">>,
+    eutil:http_post(URL, ?URLENCEDED_HEADS, Datas, [{pool, ?POOL}]).
+
+get_record(Mobile, StartTime, EndTime) ->
+    Apikey = get_conf_api_key(),
+    get_record(Apikey, Mobile, StartTime, EndTime).
+
+get_record(Apikey, Mobile, StartTime, EndTime) ->
+    Datas = #{apikey => Apikey, mobile => Mobile,
+              start_time => StartTime, end_time => EndTime},
+    get_record(Datas).
+
+get_record(Datas) ->
+    URL = <<"https://sms.yunpian.com/v2/sms/get_record.json">>,
+    eutil:http_post(URL, ?URLENCEDED_HEADS, Datas, [{pool, ?POOL}]).
+
+
+get_black_word(Text) ->
+    Apikey = get_conf_api_key(),
+    get_black_word(Apikey, Text).
+
+get_black_word(Apikey, Text) ->
+    URL = <<"https://sms.yunpian.com/v2/sms/get_black_word.json">>,
+    Datas = #{apikey => Apikey, text => Text},
+    eutil:http_post(URL, ?URLENCEDED_HEADS, Datas, [{pool, ?POOL}]).
+
+
+
+
+
+get_conf_api_key() ->
+    {ok, Apikey} = application:get_env(eyunpian, apikey),
+    Apikey.
+
+format_mobiles(Mobiles) when is_list(Mobiles) ->
+    Fun = fun(Mobile) ->
+                  eutil:to_list(Mobile)
+          end,
+    list_to_binary(string:join(lists:map(Fun, Mobiles), ",")).
+
+format_texts(Texts) when is_list(Texts) ->
+    Fun = fun(Text) ->
+                  case is_binary(Text) of
+                      true -> unicode:characters_to_list(Text);
+                      false -> Text
+                  end
+          end,
+    unicode:characters_to_binary(string:join(lists:map(Fun, Texts), ",")).
